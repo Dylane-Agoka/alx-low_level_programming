511a39f1330fdcbea11362e362ea72691a3253f5
Dylane-Agoka <dylaneagoka47@gmail.com>
2022-03-18 03:08:41 +0100

    product

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-*mul - multiple two ints
-*@a: integer
-*@b: integer
-*Return: product
+*mul - multiple two ints.
+*@a: integer.
+*@b: integer.
+*Return: product.
 */
 
 int mul(int a, int b){
